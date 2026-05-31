734b7b43098953747711a6fa710dfad96296b62d
dimayakovliev-create <dima.yakovliev@gmail.com>
2026-05-31 15:07:14 +0300

    Delete assistant directory

--- assistant/command_parser.py
+++
@@ -1,6 +0,0 @@
-#    Розбирає введений рядок на команду та аргументи. Повертає команду в нижньому регістрі та список аргументів.
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    
-    return cmd, *args
